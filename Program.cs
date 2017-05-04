9336a5d7492fed0f02565a979707b0bd6f7d54e4
Psothny Soth <maja1994@vp.pl>
2017-05-04 23:45:13 +0200

    basic feature done - creating year/month folders and pushing all unorganized files to them

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
+/// <summary>
+/// Manage last months crap by moving it to named folder! [chosenBase]//year//month//[allthecrpafiles]
+/// </summary>
 namespace MonthlyFolders
 {
     class Program
@@ -12,32 +11,80 @@ namespace MonthlyFolders
         static void Main(string[] args)
         {
             string path;
+            bool sortCrap = false;
             if (args[0] == null)
             {
                 Console.WriteLine("Add path to Monthly Folder Creation in program arguments");
                 Console.ReadKey();
             }
-            else if (args[0].Contains('\"') || args[0].Contains('\''))
+            else if (args[0].IndexOf("\"")>-1 || args[0].Contains("\'"))
             {
                 path = args[0].Substring(1, args[0].Length - 1);
             }
             else {
                 path = args[0];
-                if (!File.Exists(path))
-                    {
-                        Console.WriteLine("Path to Monthly Folder Creation is wrong or doesn't exist");
-                    }
+                if (!Directory.Exists(path))
+                 {
+                        Console.WriteLine("Path to Monthly Folder Creation directory is wrong or doesn't exist");
+                         Console.ReadKey();
+                }
                 else
                 {
-                    CheckFolders(path);
+                    if (ManageLastMontFolder(path) == true)
+                    {
+                        if (args[1] != null && bool.TryParse(args[1], out sortCrap) == true && sortCrap == true)
+                        {
+                            MoveCrap(path);
+                        }
+                    }
                 }
             }
         }
-
-        static void CheckFolders(string path)
+        /// <summary>
+        /// check if folder for last month exists, if not, make one
+        /// </summary>
+        /// <param name="path"> path to folder where programs hould manage years and months</param>
+        /// <returns>true if made new folder for last month</returns>
+        static bool ManageLastMontFolder(string path)
         {
+            int year = DateTime.Now.Year;
+            //-1 for last month
+            int month = DateTime.Now.Month-1;
+            if (!Directory.Exists(path + "//" + year + "//" + month))
+            {
+                Directory.CreateDirectory(path + "//" + year + "//" + month);
+                return true;
+            }
+            else return false;
+            
+           
+        }
+        /// <summary>
+        /// move all crap from last month to last month's folder
+        /// </summary>
+        /// <param name="path"> path to managed folderspace</param>
+        static void MoveCrap(string path)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(path);
+            foreach (string fileName in fileEntries)
+                MoveFile(fileName, path);
 
-
+        }
+        /// <summary>
+        /// move file from base folder to corresponing to creation date year/month folder
+        /// </summary>
+        /// <param name="filePath"> path to file that should be checked</param>
+        /// <param name="folderPath">path to general base folder where all the crap lies</param>
+        public static void MoveFile(string filePath, string folderPath)
+        {
+            FileInfo fi = new FileInfo(filePath);
+            if(fi.CreationTime.Year == DateTime.Now.Year && fi.CreationTime.Month == fi.CreationTime.Month)
+            {
+                string fileName = filePath.Substring(folderPath.Length);
+                File.Move(filePath, folderPath + "//" + DateTime.Now.Year + "//" + DateTime.Now.Month+fileName);
+            }
         }
     }
 }
+
